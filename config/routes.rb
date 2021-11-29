f9f77924c24bad4a2064d7c8fb646321c73920d4
Geetha Bheema <geetha.bheema01@gmail.com>
2021-11-29 23:06:43 +0800

    items create new actions complete and items controller is updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :items, only: [:index, :show, :delete]
+  resources :items, only: [:index, :show, :delete, :create]
 
   resources :users do
-    resources :items, only: [:edit, :update, :new, :create]
+    resources :items, only: [:edit, :update, :new]
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
