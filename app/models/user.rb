cd919953e837ba9c55df87885414d3bf75958b8d
Benedict Ng <benedictngbh@gmail.com>
2021-11-29 17:38:23 +0800

    models set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :carts
+  has_many :cart_items, through: :carts
   has_many :items, through: :cart_items
 end
