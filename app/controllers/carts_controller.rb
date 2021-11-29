d8952f1ff6899287ae62cbb56555801380f9b34f
Benedict Ng <benedictngbh@gmail.com>
2021-11-29 23:12:42 +0800

    pause to check pr

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,22 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update, :destroy, :mark_complete]
+  before_action :set_user, only: [:active, :history]
   def active
     @user = User.find(params[:user_id])
     @cart = @user.carts[0]
+  end
+
+  def history
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_cart
+    @cart = Cart.find(params[:id])
   end
 end
