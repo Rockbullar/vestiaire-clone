bcaeb1bb50077948e41480d2c688b3b5d51f782f
Benedict Ng <benedictngbh@gmail.com>
2021-11-29 18:03:15 +0800

    routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,15 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :items, only: [:edit, :update, :new, :create]
+    resources :carts, only: [:create, :show]
+    get '/carts/history', to: 'cards#history'
   end
-  
+
+  get '/carts/:id/checkout', to: 'cards#checkout', as: 'checkout_cart' # change cart status to sold, create new cart, update user's cart to new cart
+  post '/cart_item/:id', to: 'cart_item#create', as: 'create_cart_item' # no redirect, just flash a message
+  delete '/cart_item/:id', to: 'cart_item#destroy', as: 'destroy_cart_item' # redirect back to cart show
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
