5dfff9ba0bdff07d3f06957b22e0752155b3232d
Soh Zhen Yi <sohzhenyi@gmail.com>
2021-11-30 16:30:36 +0800

    fixed the listings to be displayed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get "items/yourlistings", to: "items#listed", as: 'items_listed' #search for user's own listings
   resources :items, only: [:index, :show, :destroy, :create]
 
   resources :users do
@@ -14,8 +15,6 @@ Rails.application.routes.draw do
   post '/carts/:id/checkout', to: 'carts#checkout', as: 'checkout_cart' # change cart status to purchased, create new cart, update user's cart to new cart
   post '/cart_item', to: 'cart_item#create', as: 'create_cart_item' # no redirect, just flash a message
   delete '/cart_item/:id', to: 'cart_item#destroy', as: 'destroy_cart_item' # redirect back to cart show
-
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
