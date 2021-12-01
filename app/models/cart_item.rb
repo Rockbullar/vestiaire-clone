bf64f05bdc1c8938668b57d34f290ae921098681
Benedict Ng <benedictngbh@gmail.com>
2021-12-01 12:17:24 +0800

    duplicate item error escaped

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,6 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :cart
 
-  validates :item, uniqueness: { scope: :cart, message: "no repeat items in cart" }
+  validates :item, uniqueness: { scope: :cart }
   # validates :item, uniqueness: true
 end
